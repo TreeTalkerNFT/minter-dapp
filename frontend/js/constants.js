717a9a5d5d4d8c58fcf1c882b89c641718d6ee06
Umair <treetalkernft@gmail.com>
2022-05-07 06:41:41 +0300

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x5dc7085F2A686b09cdC9622C0032dCeB34A27081"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
